d072193b6c58126c891f927606cb16e8bcb3dc9e
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-24 21:54:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
  puts "Choose a number between 1-6, or type 'exit' to escape"
   player_input = gets.chomp
-  comp_num = rand(6).to_s
+  comp_num = (rand(6)+1).to_s
  puts player_input
  puts comp_num
 
