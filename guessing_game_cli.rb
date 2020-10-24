ae900944db8ed1547045a2be88b74e476695091d
Marcia Freeman <marciafreeman2011@gmail.com>
2020-10-24 21:20:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,7 @@ def run_guessing_game
   if player_input == "exit"
     puts "Goodbye!"
   end
-    if player_input == comp_num
+    if player_input = comp_num
     puts "You guessed the correct number!"
   end
   
